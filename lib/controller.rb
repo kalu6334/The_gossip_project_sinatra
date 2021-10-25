d8d3c299519495575e240259282cf75c8160eb87
kalu6334 <martin.lucas76334@gmail.com>
2021-10-25 23:24:04 +0200

    first_commit

---
+++ lib/controller.rb
@@ -0,0 +1,34 @@
+require_relative 'gossip'
+
+class ApplicationController < Sinatra::Base
+
+    get '/' do 
+        erb :index , locals: {gossips: Gossip.all}
+    end
+
+    get '/gossips/new/' do 
+        erb :new_gossip
+    end
+
+    post '/gossips/new/' do 
+        # puts "Ce programme ne fait rien pour le moment, on vas donc afficher un message dans le terminal"
+        Gossip.new(params["gossip_author"],params["gossip_content"]).save
+        redirect '/'
+        puts "Salut, je suis dans le serveur"
+        puts "Ceci est le contenu du hash params: #{params}"
+        puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author: #{params["gossip_author"]}"
+        puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+        puts "Ca déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+    end
+ 
+    get '/gossips/:id' do 
+        
+        puts "voici le numéro du potin que tu veux : #{params['id']} !"
+        erb :show , locals: {gossip: Gossip.all[params[:id].to_i ], id: params[:id].to_i}
+           
+    end
+
+    
+
+   
+end
---
+++ lib/gossip.rb
@@ -0,0 +1,37 @@
+
+
+class Gossip
+
+    attr_reader :author, :content 
+
+    def initialize(author,content)
+        @content = content
+        @author = author
+    end
+
+    def save 
+                       
+        CSV.open("./db/gossip.csv", "ab") do |csv|
+        #csv << ["Mon super auteur", "Ma super description"]
+        csv << [@author, @content]
+        end
+       
+    end
+
+    def self.all 
+
+        all_gossips = []
+        CSV.read("./db/gossip.csv").each do |csv_line|
+            all_gossips << Gossip.new(csv_line[0],csv_line[1])
+        end
+        return all_gossips
+
+    end
+
+    def self.find(id)
+        return Gossip.all[id]
+    end
+
+
+end
+
